f48ab6fd6af8acc0b94420af78e92d5757492617
Fred J. Hickernell <hickernell@iit.edu>
2016-07-25 18:07:28 -0500

    added another

---
+++ ProgramsImages/TestOutMLE.m
@@ -0,0 +1,16 @@
+%% Test out MLE
+function [muhat,aMLE,err,out] = TestOutMLE(nvec,domain,f,exactInteg)
+
+if nargin < 3
+   f = @(x) cos(x(:,1) + x(:,2)); exactInteg = -cos(2) + 2*cos(1) - 1;
+   if nargin < 2
+      domain = [0 0;1 1];
+      if nargin < 1
+         nvec = 2.^6';
+      end
+   end
+end
+[muhat,out] = cubMLE(f,nvec,domain);
+aMLE = out.aMLE;
+err = abs(exactInteg - muhat);
+end
