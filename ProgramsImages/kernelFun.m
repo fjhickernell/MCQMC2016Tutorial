42bbbc0af4c84dd6841c2a16e9624502bbc9f662
jagadeesr@gmail.com <jagadeesr@gmail.com>
2017-01-20 22:48:41 -0600

    Restored the Bayesian cubature changes to my branch

--- ProgramsImages/kernelFun.m
+++ ProgramsImages/kernelFun.m
@@ -1,4 +1,4 @@
-function [K,kvec,k0] = kernelFun(x,whKer,shape,domain)
+function [K,kvec,k0] = kernelFun(x,whKer,shape,domain,BernPolynX,BernPolynOrder)
 [nx,d] = size(x);
 if nargin < 4
    domain = [zeros(1,d); ones(1,d)];
@@ -9,7 +9,10 @@ if nargin < 4
       end
    end
 end
-K = ones(nx);
+
+if ~strcmp(whKer,'Fourier')
+    K = ones(nx);
+end
 if strcmp(whKer,'sqExp') %all wrong
    kvec = ones(nx,1)*(sqrt(pi)/(2*shape))^d;
    for k = 1:d;
@@ -29,6 +32,23 @@ elseif strcmp(whKer,'Mat1')
       kvec = kvec.*(2 - exp(-tempc).*(1+tempc/2) ...
           - exp(-tempb).*(1+tempb/2));
    end
+elseif strcmp(whKer,'Fourier')
+    r = BernPolynOrder;
+	theta = shape;
+	kvec = ones(nx,1);
+	k0 = 1.0;
+	constMult = -theta^r*(-1)^(r/2)*(2*pi)^r/factorial(r);
+    bernX = BernPolynX*constMult;
+
+    cvec = prod((1.0+bernX),2);
+    % no need to explicitly create the matrix
+    % circulant matrix can be recovered with just one vector
+    K = cvec';
+end
+    
 end
 
-end
+
+
+
+
